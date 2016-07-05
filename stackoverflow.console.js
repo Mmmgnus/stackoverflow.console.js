a60b14fd59e8a394c4d21082a30cf0a452822b13
Magnus Fredlundh <magnusfredlundh@gmail.com>
2016-07-05 23:00:39 +0200

    Changed the name of the JSON file and some refactoring.

--- stackoverflow.console.js
+++ stackoverflow.console.js
@@ -2,18 +2,33 @@
 	"use strict";
 
 	var xhr = new XMLHttpRequest();
+
 	xhr.addEventListener("load", xhr_load);
-	xhr.open("GET", "stackoverflow.console.json");
+	xhr.open("GET", "info.json");
 	xhr.send();
 
 	function xhr_load (event) {
-		var json = JSON.parse(event.currentTarget.response);
+		var info = JSON.parse(event.currentTarget.response);
+		var entries = info.stackoverflow;
 		var group_headline_style = 'font-weight: normal;';
 
-		console.group('%cThis site used these %cStackoverflow%c entries', group_headline_style, 'background:#f69c55;color:#fff;padding:5px;', group_headline_style);
+		if (!entries && !entries.length) {
+			return;
+		}
 
-		json.forEach(function(entry) {
-			console.log('%c' + entry.title + '\n%c' + entry.url, 'font:16px/1.5 arial;font-weight:bold;', 'font-weight:normal;color:blue;');
+		console.group(
+			'%cThis site used these %cStackoverflow%c entries',
+			group_headline_style,
+			'background:#f69c55;color:#fff;padding:5px;',
+			group_headline_style
+		);
+
+		entries.forEach(function(entry) {
+			console.log(
+				'%c' + entry.title + '\n%c' + entry.url,
+				'font:16px/1.5 arial;font-weight:bold;',
+				'font-weight:normal;color:blue;'
+			);
 		});
 
 		console.groupEnd();
